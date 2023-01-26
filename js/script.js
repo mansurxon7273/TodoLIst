06ab0abc66f943eef91c942a9d0eccd6d3fafa73
mannsurxon7273 <rahmonaliyev2311@gmail.com>
2023-01-26 14:32:57 +0500

    main

--- js/script.js
+++ js/script.js
@@ -6,30 +6,71 @@ const input = document.querySelector("input"),
   updateBtm = document.querySelector(".update");
 let id = 1,
   upTest = false;
+for (let i = 1; i <= localStorage.length; i++) {
+  if (localStorage.getItem(`${i}`)) {
+    let item = document.createElement("div");
+    item.classList.add("item", `item__${i}`);
+    item.innerHTML = `
+    <span class="id">${i}</span>
+    <span class="username">${
+      JSON.parse(localStorage.getItem(`${i}`)).username
+    }</span>
+    <span class="bottons">
+      <i class="fa-sharp fa-solid fa-trash"></i>
+      <i class="fa-sharp fa-solid fa-pen-to-square"></i>
+    </span>
+    `;
+    item.addEventListener("click", function (e) {
+      //delete
+      if (e.target.className == "fa-sharp fa-solid fa-trash") {
+        this.remove();
+        localStorage.removeItem(`${i}`);
+        console.log(i);
+      }
+      // update
+      else if (e.target.className == "fa-sharp fa-solid fa-pen-to-square") {
+        let value = item.childNodes[3].textContent;
+        input.value = value;
+        btn.style.display = "none";
+        updateBtm.style.display = "block";
+        updateBtm.addEventListener("click", function () {
+          item.childNodes[3].innerHTML = input.value;
+          input.value = "";
+          this.style.display = "none";
+          btn.style.display = "block";
+        });
+      }
+    });
+    read.appendChild(item);
+  }
+  id = i;
+}
 btn.addEventListener("click", function () {
   if (input.value === "") {
     alert("Siz hech narsa kirtmadingiz!");
     // input.setAttribute('autofocus', '')
   } else {
-    localStorage
-      .setItem
-      // `${id}`,
-      // JSON.stringify({ username: `${input.value}` })
-      ();
+    // localStorage.setItem(`${id}`, JSON.stringify({ username: `${input.value}))
+    localStorage.setItem(
+      `${id}`,
+      JSON.stringify({ username: `${input.value}` })
+    );
     let item = document.createElement("div");
     item.classList.add("item", `item__${id}`);
     item.innerHTML = `
-  <span class="id">${id}</span>
-  <span class="username">${input.value}</span>
-  <span class="bottons">
-    <i class="fa-sharp fa-solid fa-trash"></i>
-    <i class="fa-sharp fa-solid fa-pen-to-square"></i>
-  </span>
-  `;
+    <span class="id">${id}</span>
+    <span class="username">${input.value}</span>
+    <span class="bottons">
+      <i class="fa-sharp fa-solid fa-trash"></i>
+      <i class="fa-sharp fa-solid fa-pen-to-square"></i>
+    </span>
+    `;
     item.addEventListener("click", function (e) {
       //delete
       if (e.target.className == "fa-sharp fa-solid fa-trash") {
         this.remove();
+        localStorage.removeItem(`${id}`);
+        console.log(id);
       }
       // update
       else if (e.target.className == "fa-sharp fa-solid fa-pen-to-square") {
@@ -50,6 +91,3 @@ btn.addEventListener("click", function () {
     input.value = "";
   }
 });
-// const obj = { name: "Mansurbek", age: 19 };
-// localStorage.setItem("1", JSON.stringify(obj));
-// console.log(localStorage.getItem("1"));
